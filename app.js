102c2d9b81cc11861c8acb2a68cda3ca252e8ff4
Tony Cook <tony_cook@outlook.com>
2021-10-25 12:50:54 +1300

    loop exercises added to JS

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-logo = document.getElementById("logo")
+// Git push
+
+// logo = document.getElementById("logo")
 
 
-function changeBackground () {
-    document.body.style.background = "white"
+// function changeBackground () {
+//     document.body.style.background = "white"
+// }
+
+// logo.addEventListener('mouseover',changeBackground);
+// logo.addEventListener('mouseout', () => {document.body.style.background = "black"})
+
+
+// Loops
+// Exercise 1
+// 1. Now try running a loop that does a console log of the numbers 1 – 10
+
+// console.log ("Testing 1,2")
+
+// for (let i=1; i<=10; i++) {
+//     console.log(i);
+// }
+
+// Exercise 2
+// Try running a loop that does a console log of the numbers 10 – 1 in that order.
+
+for (let i=10; i>=1; i--) {
+    console.log(i);
 }
-
-logo.addEventListener('mouseover',changeBackground);
-logo.addEventListener('mouseout', () => {document.body.style.background = "black"})
